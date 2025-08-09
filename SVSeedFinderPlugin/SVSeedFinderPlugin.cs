f4da72954e22e3b1add305dad7f63078b3dae382
bdawg1989 <80122551+hexbyt3@users.noreply.github.com>
2025-08-09 16:53:30 -0500

    Update shortcut/disable for other saves

--- SVSeedFinderPlugin/SVSeedFinderPlugin.cs
+++ SVSeedFinderPlugin/SVSeedFinderPlugin.cs
@@ -18,6 +18,7 @@ public sealed class SVSeedFinderPlugin : IPlugin
     // Initialized on plugin load
     public ISaveFileProvider SaveFileEditor { get; private set; } = null!;
     public IPKMView PKMEditor { get; private set; } = null!;
+    private ToolStripMenuItem? PluginMenuItem { get; set; }
 
     public void Initialize(params object[] args)
     {
@@ -45,14 +46,14 @@ public sealed class SVSeedFinderPlugin : IPlugin
 
     private void AddPluginControl(ToolStripDropDownItem tools)
     {
-        var ctrl = new ToolStripMenuItem(Name)
+        PluginMenuItem = new ToolStripMenuItem(Name)
         {
-            ShortcutKeys = Keys.Control | Keys.F,
+            ShortcutKeys = Keys.Control | Keys.Shift | Keys.V,
             Image = CreateTeraCrystalIcon()
         };
 
-        ctrl.Click += (_, _) => ShowSeedFinderForm();
-        tools.DropDownItems.Add(ctrl);
+        PluginMenuItem.Click += (_, _) => ShowSeedFinderForm();
+        tools.DropDownItems.Add(PluginMenuItem);
         Console.WriteLine($"{Name} added menu item.");
     }
 
@@ -100,6 +101,15 @@ public sealed class SVSeedFinderPlugin : IPlugin
     public void NotifySaveLoaded()
     {
         Console.WriteLine($"{Name} was notified that a Save File was just loaded.");
+        
+        if (PluginMenuItem != null)
+        {
+            var sav = SaveFileEditor.SAV;
+            // Enable only for Scarlet/Violet games
+            PluginMenuItem.Enabled = sav.Version == GameVersion.SL || 
+                                    sav.Version == GameVersion.VL || 
+                                    sav.Version == GameVersion.SV;
+        }
     }
 
     public bool TryLoadFile(string filePath)
